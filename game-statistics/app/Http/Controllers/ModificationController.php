ad5a013a022b607f7213bdbd84ebc9404a18b58d
Josip Bošnjak <jbosnjak3@gmail.com>
2026-05-07 19:36:50 +0200

    added modification controller with routes for index pages

---
+++ game-statistics/app/Http/Controllers/ModificationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Models\Modification;
+use App\Models\Sequel;
+
+class ModificationController extends Controller
+{
+    public function index(Game $game){
+        $modifications=Modification::with('games')->orderBy('id')->paginate(5);
+        return view("profile.modification.index",[
+            'modifications'=>$modifications,
+            'game'=>$game
+        ]);
+    }
+    public function seqIndex(Game $game,Sequel $sequel){
+        $modifications=Modification::with('sequels')->orderBy('id')->paginate(5);
+        return view("profile.modification.index",[
+            'modifications'=>$modifications,
+            'game'=>$game,
+            'sequel'=>$sequel
+        ]);
+    }
+}
