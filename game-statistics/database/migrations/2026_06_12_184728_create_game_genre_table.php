2ef852ab70a9e18e7abc18901edf7c71f0fcc606
Josip Bošnjak <jbosnjak3@gmail.com>
2026-06-12 21:08:22 +0200

    added one to more connection, additional table to one game or sequel to have one or more genres not only one genre. added model in mvb and png formar

---
+++ game-statistics/database/migrations/2026_06_12_184728_create_game_genre_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_genre', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("game_id")->constrained("game")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("sequel_id")->nullable()->constrained("sequel")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('game_genre', function (Blueprint $table) {
+          Schema::dropIfExists('game_genre');
+        });
+    }
+};
