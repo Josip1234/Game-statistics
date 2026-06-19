b786b4a210f66c03c340f7e43ad8c4eb3760561c
Josip Bošnjak <jbosnjak3@gmail.com>
2026-06-19 11:16:18 +0200

    element in checkbox is now are being removed if selected value is in ist and checked. need to implement to return that value into checbox list if something else is selected and that selected value to be removed from checkbox list.

--- game-statistics/public/js/selectedfirstgenre.js
+++ game-statistics/public/js/selectedfirstgenre.js
@@ -1,15 +1,39 @@
+let removedElement="";
+
+window.onload= function(){
+  removedElement=removeCheckedValueFromCheckbox();
+  alert(removedElement);
+}
 function getValues(){
 //chosen genre id
 let genreSelectedValues = document.getElementById("genre").value;
-//get all li elements
-let li=document.getElementsByTagName("li");
-let value= li.item(genreSelectedValues-1);
 
-//let parent=document.getElementById("checkboxValues");
-//let removed = parent.removeChild(value);
-
-if (value.parentNode) {
-  value.parentNode.removeChild(value);
+    document.getElementById(genreSelectedValues).remove();
 }
 
+function removeCheckedValueFromCheckbox(){
+  let removedElement="";
+  let genreSelectedValues = document.getElementById("genre").value;
+  //if there ia a checked value remove it from chexbox form
+var ulElement=document.getElementById("checkboxValues");
+var lists=ulElement.getElementsByTagName("li");
+for (let index = 0; index < lists.length; index++) {
+  const element = lists[index];
+  const div = element.getElementsByTagName("div");
+  for( j=0;j<div.length;j++){
+      const divContent=div[j].getElementsByTagName("input");
+      
+      for (let dc = 0; dc < divContent.length; dc++) {
+        const isChecked = divContent[dc].getAttribute("checked");
+        if(isChecked!==null){
+            removedElement=document.getElementById(genreSelectedValues).getHTML();
+            document.getElementById(genreSelectedValues).remove();
+        }
+        
+      }
+      
+  }
 }
+return removedElement;
+}
+
