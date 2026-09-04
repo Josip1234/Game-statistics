e57ed446377b4dd714bf1c05131f2d3a330dff36
Josip Bošnjak <jbosnjak3@gmail.com>
2026-09-04 10:58:08 +0200

    Update CheckDatabaseConnection.php
    updated middleware instead returning response added abort exception with response errors 403 instead one of 404 which is not possible to customize to show error with default laravel css

--- game-statistics/app/Http/Middleware/CheckDatabaseConnection.php
+++ game-statistics/app/Http/Middleware/CheckDatabaseConnection.php
@@ -18,11 +18,11 @@ class CheckDatabaseConnection
     {
         //DB CONNECTION MUST BE MYSQL
         if(env("DB_CONNECTION")!="mysql"){
-            return response('Database connection is not mysql. Access denied.',403);
+            abort(403,'Database connection is not mysql. Access denied.');
         }
         //ENV VARIABLES FOR DATABASE CANNOT BE EMPTY
         if(env("DB_HOST")=="" || env("DB_DATABASE")=="" || env("DB_USERNAME")=="" || env("DB_PASSWORD")==""){
-             return response('Missing one of the database parameters.',404);
+             abort(403,'Missing one of the database parameters.');
         }
     
        /* Log::info('Globalni',[ 
